b42556ed5e8b3463bd23cad759eff15c992caedc
Omee <ughritlahre22@gmail.com>
2023-04-06 15:55:12 +0530

    update event

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -12,7 +12,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class CreateComponent {
   btnupdate: boolean = true;
   btnpost: boolean = false;
-  selectedFile: any;
+  url: any;
+  msg="";
 
   constructor(
     private demo: DemoService,
@@ -111,7 +112,26 @@ export class CreateComponent {
       }
     });
   }
-  onFileChange(event: any) {
-    this.selectedFile = event.target.files[0]
+  selectFile(event:any){
+
+    if(!event.target.files[0] || event.target.files[0].length == 0) {
+			this.msg = 'You must select an image';
+			return;
+		}
+		
+		var mimeType = event.target.files[0].type;
+		
+		if (mimeType.match(/image\/*/) == null) {
+			this.msg = "Only images are supported";
+			return;
+		}
+		
+		var reader = new FileReader();
+		reader.readAsDataURL(event.target.files[0]);
+		
+		reader.onload = (_event) => {
+			this.msg = "";
+			this.url = reader.result; 
+		}
   }
 }
